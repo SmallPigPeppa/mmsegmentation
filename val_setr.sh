60fab7df5fec535fe98499747ef048b38dda5f09
lwz <905328134@qq.com>
2024-04-16 16:08:29 +0800

    initial

--- val_setr.sh
+++ val_setr.sh
@@ -1,3 +1,9 @@
-CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-512x512.py
+#CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-512x512.py
+#CHECKPOINT_FILE=ckpt/setr_naive_512x512_160k_b16_ade20k_20210619_191258-061f24f5.pth
+#python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE}
+
+
+
+CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256.py
 CHECKPOINT_FILE=ckpt/setr_naive_512x512_160k_b16_ade20k_20210619_191258-061f24f5.pth
 python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE}
