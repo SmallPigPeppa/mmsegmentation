bd3015e15241ba6b466880d6a1c3aa35032b5c96
lwz <905328134@qq.com>
2024-04-18 18:11:18 +0800

    initial

--- val_setr_city.sh
+++ val_setr_city.sh
@@ -45,7 +45,7 @@ for size in 384x384 192x192
 do
     # Set variables based on the current size
     CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb1-80k_cityscapes-${size}_imgresize.py"
-    CHECKPOINT_FILE="ckpt/ms_ckpt/setr_naive_vit-large_8x1_${size}_80k_cityscapes_20211123_000505-20728e80_bi.pth"
+    CHECKPOINT_FILE="ckpt/setr_naive_vit-large_8x1_768x768_80k_cityscapes_20211123_000505-20728e80.pth"
     WORK_DIR="work_dirs/setr-city/setr_vit-l_naive_8xb1-80k_cityscapes-${size}_imgresize"
 
     # Execute the test command
