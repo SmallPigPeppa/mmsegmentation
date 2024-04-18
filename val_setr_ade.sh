3a8a405cf73f9fc97e6e1820cf9784cafafe40c3
lwz <905328134@qq.com>
2024-04-18 23:16:49 +0800

    initial

--- val_setr_ade.sh
+++ val_setr_ade.sh
@@ -1,52 +1,39 @@
-CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-512x512.py
-CHECKPOINT_FILE=ckpt/setr_naive_512x512_160k_b16_ade20k_20210619_191258-061f24f5.pth
-python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE}
+# Vanilla
+for size in 512x512 256x256 128x128
+do
+    # Set variables based on the current size
+    CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-${size}.py"
+    CHECKPOINT_FILE="ckpt/setr_naive_512x512_160k_b16_ade20k_20210619_191258-061f24f5.pth"
+    WORK_DIR="work_dirs/setr-ade/setr_vit-l_naive_8xb2-160k_ade20k-${size}_vanilla"
 
-#CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256.py
-#CHECKPOINT_FILE=ckpt/setr_naive_512x512_160k_b16_ade20k_20210619_191258-061f24f5.pth
-#python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE}
+    # Execute the test command
+    echo "Running test for size $size"
+    python tools/test.py "${CONFIG_FILE}" "${CHECKPOINT_FILE}" --work-dir "${WORK_DIR}"
+done
 
-#CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256_pi.py
-#CHECKPOINT_FILE=ckpt/ms_ckpt/setr_naive_256x256_160k_b16_ade20k_20210619_191258-061f24f5_pi.pth
-#python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE}
+# bi resize
+for size in 256x256 128x128
+do
+    # Set variables based on the current size
+    CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-${size}_bipi.py"
+    CHECKPOINT_FILE="ckpt/ms_ckpt/setr_naive_${size}_160k_b16_ade20k_20210619_191258-061f24f5_bi.pth"
+    WORK_DIR="work_dirs/setr-ade/setr_vit-l_naive_8xb2-160k_ade20k-${size}_bi"
 
-#CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256_bi.py
-#CHECKPOINT_FILE=ckpt/ms_ckpt/setr_naive_256x256_160k_b16_ade20k_20210619_191258-061f24f5_bi.pth
-#python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE}
-
-
-CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256_bipi.py
-CHECKPOINT_FILE=ckpt/ms_ckpt/setr_naive_256x256_160k_b16_ade20k_20210619_191258-061f24f5_bi.pth
-python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} --work-dir work_dirs/configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256_bi
-
-
-CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256_bipi.py
-CHECKPOINT_FILE=ckpt/ms_ckpt/setr_naive_256x256_160k_b16_ade20k_20210619_191258-061f24f5_pi.pth
-python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} --work-dir work_dirs/configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256_pi
-
-
-CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256.py
-CHECKPOINT_FILE=ckpt/setr_naive_512x512_160k_b16_ade20k_20210619_191258-061f24f5.pth
-python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} --work-dir work_dirs/configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-256x256
+    # Execute the test command
+    echo "Running test for size $size"
+    python tools/test.py "${CONFIG_FILE}" "${CHECKPOINT_FILE}" --work-dir "${WORK_DIR}"
+done
 
 
+# pi resize
+for size in 256x256 128x128
+do
+    # Set variables based on the current size
+    CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-${size}_bipi.py"
+    CHECKPOINT_FILE="ckpt/ms_ckpt/setr_naive_${size}_160k_b16_ade20k_20210619_191258-061f24f5_pi.pth"
+    WORK_DIR="work_dirs/setr-ade/setr_vit-l_naive_8xb2-160k_ade20k-${size}_pi"
 
-
-
-
-CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-128x128_bipi.py
-CHECKPOINT_FILE=ckpt/ms_ckpt/setr_naive_128x128_160k_b16_ade20k_20210619_191258-061f24f5_bi.pth
-python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} --work-dir work_dirs/configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-128x128_bi
-
-
-CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-128x128_bipi.py
-CHECKPOINT_FILE=ckpt/ms_ckpt/setr_naive_128x128_160k_b16_ade20k_20210619_191258-061f24f5_pi.pth
-python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} --work-dir work_dirs/configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-128x128_pi
-
-
-CONFIG_FILE=configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-128x128.py
-CHECKPOINT_FILE=ckpt/setr_naive_512x512_160k_b16_ade20k_20210619_191258-061f24f5.pth
-python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} --work-dir work_dirs/configs/setr/setr_vit-l_naive_8xb2-160k_ade20k-128x128
-
-
-
+    # Execute the test command
+    echo "Running test for size $size"
+    python tools/test.py "${CONFIG_FILE}" "${CHECKPOINT_FILE}" --work-dir "${WORK_DIR}"
+done
