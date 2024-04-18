a01e32fe2e6431f8a3e15ef2d0e83448c38f5138
lwz <905328134@qq.com>
2024-04-18 16:37:22 +0800

    initial

--- val_setr_city.sh
+++ val_setr_city.sh
@@ -1,8 +1,8 @@
-for size in 192x192
+for size in 768x768 384x384 192x192
 do
     # Set variables based on the current size
     CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb1-80k_cityscapes-${size}.py"
-    CHECKPOINT_FILE="ckpt/setr_naive_vit-large_8x1_${size}_80k_cityscapes_20211123_000505-20728e80.pth"
+    CHECKPOINT_FILE="ckpt/setr_naive_vit-large_8x1_768x768_80k_cityscapes_20211123_000505-20728e80.pth"
     WORK_DIR="work_dirs/setr-city/setr_vit-l_naive_8xb1-80k_cityscapes-${size}"
 
     # Execute the test command
@@ -10,31 +10,31 @@ do
     python tools/test.py "${CONFIG_FILE}" "${CHECKPOINT_FILE}" --work-dir "${WORK_DIR}"
 done
 
-#
-#
-#for size in 384x384 192x192
-#do
-#    # Set variables based on the current size
-#    CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb1-80k_cityscapes-${size}.py"
-#    CHECKPOINT_FILE="ckpt/ms_ckpt/setr_naive_vit-large_8x1_${size}_80k_cityscapes_20211123_000505-20728e80_bi.pth"
-#    WORK_DIR="work_dirs/setr-city/setr_vit-l_naive_8xb1-80k_cityscapes-${size}_bi"
-#
-#    # Execute the test command
-#    echo "Running test for size $size"
-#    python tools/test.py "${CONFIG_FILE}" "${CHECKPOINT_FILE}" --work-dir "${WORK_DIR}"
-#done
-#
-#
-#
-#for size in 384x384 192x192
-#do
-#    # Set variables based on the current size
-#    CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb1-80k_cityscapes-${size}.py"
-#    CHECKPOINT_FILE="ckpt/ms_ckpt/setr_naive_vit-large_8x1_${size}_80k_cityscapes_20211123_000505-20728e80_pi.pth"
-#    WORK_DIR="work_dirs/setr-city/setr_vit-l_naive_8xb1-80k_cityscapes-${size}_pi"
-#
-#    # Execute the test command
-#    echo "Running test for size $size"
-#    python tools/test.py "${CONFIG_FILE}" "${CHECKPOINT_FILE}" --work-dir "${WORK_DIR}"
-#done
-#
+
+
+for size in 384x384 192x192
+do
+    # Set variables based on the current size
+    CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb1-80k_cityscapes-${size}.py"
+    CHECKPOINT_FILE="ckpt/ms_ckpt/setr_naive_vit-large_8x1_${size}_80k_cityscapes_20211123_000505-20728e80_bi.pth"
+    WORK_DIR="work_dirs/setr-city/setr_vit-l_naive_8xb1-80k_cityscapes-${size}_bi"
+
+    # Execute the test command
+    echo "Running test for size $size"
+    python tools/test.py "${CONFIG_FILE}" "${CHECKPOINT_FILE}" --work-dir "${WORK_DIR}"
+done
+
+
+
+for size in 384x384 192x192
+do
+    # Set variables based on the current size
+    CONFIG_FILE="configs/setr/setr_vit-l_naive_8xb1-80k_cityscapes-${size}.py"
+    CHECKPOINT_FILE="ckpt/ms_ckpt/setr_naive_vit-large_8x1_${size}_80k_cityscapes_20211123_000505-20728e80_pi.pth"
+    WORK_DIR="work_dirs/setr-city/setr_vit-l_naive_8xb1-80k_cityscapes-${size}_pi"
+
+    # Execute the test command
+    echo "Running test for size $size"
+    python tools/test.py "${CONFIG_FILE}" "${CHECKPOINT_FILE}" --work-dir "${WORK_DIR}"
+done
+
