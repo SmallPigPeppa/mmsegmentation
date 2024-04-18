cf157cdc7fe294eb363a06f1658898fcb4d9aad1
lwz <905328134@qq.com>
2024-04-19 00:11:00 +0800

    initial

--- configs/_base_/datasets/ade20k_128x128_imgresize.py
+++ configs/_base_/datasets/ade20k_128x128_imgresize.py
@@ -19,6 +19,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     # dict(type='Resize', scale=(2048, 512), keep_ratio=True),
     dict(type='Resize', scale=(512, 128), keep_ratio=True),
+    dict(type='Resize', scale=(2048, 512), keep_ratio=True),
     # add loading annotation after ``Resize`` because ground truth
     # does not need to do resize data transform
     dict(type='LoadAnnotations', reduce_zero_label=True),
